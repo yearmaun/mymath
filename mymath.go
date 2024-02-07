ed14478212617366615b2397fa3ab403f9863ca3
Alexander Patkin <al.pat@list.ru>
2024-02-07 23:58:08 +0300

    2

--- mymath.go
+++ mymath.go
@@ -2,7 +2,34 @@ package mymath
 
 import "math"
 
-// Sqrt - функция для вычисления квадратного корня из числа
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
 }
